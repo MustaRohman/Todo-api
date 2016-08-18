1bd06b5eec61a17c5f3c6fea822ebfdc6968cd79
Musta Rohman <mustarohman@gmail.com>
2016-08-18 21:43:11 +0100

    Added underscore package. Added validations and refactoring

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
+
+
 var PORT = process.env.PORT || 3000;
 var todos = [];
+var categories = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
@@ -18,17 +23,10 @@ app.get('/todos', function (req, res) {
 // GET /todos/id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchTodo;
-	todos.forEach(function (item) {
-		if (item.id === todoId) {
-			matchTodo = item;
-			return;
-		}
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	})
-
-	if (matchTodo) {
-		res.json(matchTodo);
+	if (matchedTodo) {
+		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
@@ -36,11 +34,26 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) 
+		|| body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	body.id = todoNextId++;
 	todos.push(body);
 
+	res.json(body);
+})
+
+// POST /categories
+app.post('/categories', function (req, res) {
+	var body = req.body;
+	categories.push(body);
+	console.log(categories);
 	res.json(body);
 })
 
