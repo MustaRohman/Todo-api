ee42de1fa5b757b210982cc7ab6f057eda16bd95
Musta Rohman <mustarohman@gmail.com>
2016-08-24 00:41:07 +0100

    Converted GET /todos to sequelize

--- server.js
+++ server.js
@@ -19,52 +19,53 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&q=work
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		console.log('filtering completed todos...');
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		console.log('filtering uncompleted todos...');
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
+	}
+
+	console.log(where);
+
+	db.todo.findAll({
+		where: where
+	}).then(
+		function(todos) {
+			res.json(todos);
+		},
+		function(e) {
+			res.status(500).send();
 		})
-	}
-	res.json(filteredTodos);
 })
 
 // GET /todos/id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	
-	db.todo.findById(todoId).then(function (todo) {
-		if (!!todo) {
-			res.json(todo.toJSON());
-		} else {
-			res.status(404).send();
-		}
-	}, function (e) {
+
+	db.todo.findById(todoId).then(function(todo) {
+		res.json(todo.toJSON());
+	}, function(e) {
 		res.status(500).json(e);
 	})
-	
+
 })
 
 // POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function (todo) {
+	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	}, function (e) {
+	}, function(e) {
 		res.status(400).json(e);
 	})
 })
