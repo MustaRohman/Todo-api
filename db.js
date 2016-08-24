853a16ba4e9ec3bb7c0d824325af2e278a1cd2dd
Musta Rohman <mustarohman@gmail.com>
2016-08-24 07:34:39 +0100

    Added postgres for production

--- db.js
+++ db.js
@@ -1,8 +1,20 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env === 'production') {
+	// This will be true if running on heroku
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
+
+
 
 var db = {}; // We can return multiple properties thru module.exports using object
 
